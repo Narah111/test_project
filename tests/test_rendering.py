376da5ad364fb10f188afb338cf33d93c033c5eb
almafrank <alma.k.frank@hotmail.com>
2025-04-14 19:30:45 +0200

    Add Unittest rendering.py
    Add unittest test_get_users_visit_history

--- tests/test_rendering.py
+++ tests/test_rendering.py
@@ -19,3 +19,52 @@ def test_get_welcome_message_to_user(request_mock_basic_html_page):
     request_mock_basic_html_page.assert_called_once_with("Welcome","Welcome, you are visitor number 5")
 
 
+#2. Den generarar neråt och upp
+@patch('rendering.get_html_end_block')#sist
+@patch('rendering.to_text_paragraph')
+@patch('rendering.to_heading_line')
+@patch('rendering.get_html_start_block')# först
+def test_get_users_visit_history(request_mock_start_block, request_mock_heading,request_mock_paragraph,request_mock_html_end_block):
+
+    #Given html
+
+    request_mock_start_block.return_value="<html>"
+    request_mock_heading.return_value="<h1>Visit history</h1>"
+    def mock_text_paragraph(text_paragraph):
+        return f"<p>{text_paragraph}</p>"
+    
+    request_mock_paragraph.side_effect=mock_text_paragraph
+    request_mock_html_end_block.return_value="</html>"
+
+    #value of user_visits
+
+    user_visit_id=[
+        {
+            "id":1,
+            "timestamp":"2025-04-11 13:00 00",
+            "ip":"127.0.0.1",
+            "user_agent":"Mozilla/5.0"
+        },
+        {
+            "id":2,
+            "timestamp":"2025-04-11 17:00 00",
+            "ip":"127.0.0.1",
+            "user_agent":"Mozilla/5.0"
+
+        }
+    ]
+
+
+
+    #When retreving the userid and html from function"format_visit_history"
+    result_mock_value_user_history=format_visit_history(user_visit_id)
+
+    #Then
+    #heading-line
+   
+    assert_that(result_mock_value_user_history,contains_string("Visit history"))
+    assert_that(result_mock_value_user_history,contains_string((f"- {user_visit_id[0]['timestamp']}: Visit #{user_visit_id[0]['id']}\n")))
+    assert_that(result_mock_value_user_history,contains_string((f"- {user_visit_id[1]['timestamp']}: Visit #{user_visit_id[1]['id']}\n")))
+    
+
+
