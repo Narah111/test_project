19434e4676bfe7cebd265531db804cf77abe2060
Narah111 <rahmanzadan@gmail.com>
2025-04-15 21:59:25 +0200

    i have tried to fix test_visits_invalid_to in test_main.py but i couldnt fix it. I push in my branch so if someone else can fix it. The problem is to_error_message dont mocks.

--- tests/unittest/test_main.py
+++ tests/unittest/test_main.py
@@ -1,8 +1,17 @@
 import pytest
 from datetime import datetime 
-
+from datetime import date
 from main import app
 
+# @pytest.fixture(autouse=True)
+# def mock_db_calls(mocker):
+#     # Mocka alla funktioner som använder databaskopplingen
+#     mocker.patch("db.get_db_connection", return_value=None)
+#     mocker.patch("db.get_visit_by_id", return_value=None)
+#     mocker.patch("db.get_all_visits", return_value=[])
+#     mocker.patch("db.add_visit", return_value={})
+#     mocker.patch("db.format_visit_history", return_value="formatted history")
+#     mocker.patch("db.init_db", return_value=None)
 
 #1 function
 def test_root_route(mocker):
@@ -43,7 +52,7 @@ def test_visits_no_data(mocker):
     mock_format.assert_called_once()  # Ensure that format_visit_history was called once\
 
 
-#2 function 2 - with_from_and_to
+# #2 function 2 - with_from_and_to
 def test_visit_with_from_and_to(mocker):
     fake_visits = [
         {"timestamp": datetime(2023, 12, 31)},
@@ -77,7 +86,7 @@ def test_visits_with_data(mocker):
     assert b"formatted" in res.data
     mock_format.assert_called_once()
 
-    mock_format.assert_called_with([{"timestamp": datetime(2024, 1, 10)}]) 
+    mock_format.assert_called_with([{"timestamp": datetime(2024, 1, 10)}])
    
 
 #2 4 - function invalid for
@@ -94,11 +103,12 @@ def test_visits_invalid_from(mocker):
 
 #2 function invalid to
 def test_visits_invalid_to(mocker):
-   
+    #mock_db_conn = mocker.patch("get_db_connection", return_value="dummy_connection")
     mock_format = mocker.patch("main.to_error_message", return_value=("<p>Invalid 'to' date format. Use ISO format (YYYY-MM-DD).</p>", 400))
 
     with app.test_client() as client:
         res = client.get("/visits?to=not-a-date")
-        
+    
+    assert mock_format.called
     assert res.status_code == 400
-   
+    assert b"Invalid 'to' date format" in res.data 
