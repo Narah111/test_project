eca5425ecc879d770449e1c2e606e88d6a79256b
jiadai <yayamayajd@gmail.com>
2025-04-17 16:51:49 +0200

    add the visit inter-test, happy and sad case

--- tests/integrationtest/test_integration_test_main_alma.py
+++ tests/integrationtest/test_integration_test_main_alma.py
@@ -1,9 +1,15 @@
-from datetime import datetime, timedelta
 from hamcrest import *
-import html
 
-import pytest
-from db import get_all_visits
 
-def test_visit_with_valid_id(client):
-    id = 
+def test_visit_with_valid_id(client,visits):
+    response = client.get("/visit/1")
+    assert_that(response.status_code, 200)
+    html = response.data.decode()
+    assert_that(html,contains_string("TestSuiteAgent1"))
+
+def test_visit_with_invalid_id(client,visits):
+    response = client.get("/visit/999")
+    assert_that(response.status_code, 404)
+    html = response.data.decode()
+    assert_that(html,contains_string("Visit not found"))
+
